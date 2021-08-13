196ac3526df4c4c453ce685642aa5d5f5b4c270f
Vlasis Pitsios <vlasis.pitsios@vodafone.com>
2021-08-13 13:09:36 +0300

    Chapter 1: Using ruby

---
+++ chapter_1/using_ruby.rb
@@ -0,0 +1,12 @@
+=begin
+   You can run ruby command from terminal. Examples
+    $ ruby -e 'puts 1+5'
+    6
+
+    $ ruby -e 'puts 1==5'
+    false
+=end
+
+# one line comment
+puts "Vlasis"
+puts "Gianna"
